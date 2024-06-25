44b781c4ddacfda8fd4a14ac14d8f0a134930736
phupwintkai <phupwintkhaing@mm.cybermissions.co.jp>
2024-06-25 10:27:40 +0630

    format text

--- lib/screens/unreadMessage/unreadWidget/un_read_gp.dart
+++ lib/screens/unreadMessage/unreadWidget/un_read_gp.dart
@@ -54,109 +54,109 @@ class _UnReadDirectGpState extends State<UnReadDirectGroup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPriamrybackground,
-        body:ListView.builder(
-          itemCount:snapshot!.unreadGpMsg!.length,
-          itemBuilder:(context,index){
+        backgroundColor: kPriamrybackground,
+        body: ListView.builder(
+          itemCount: snapshot!.unreadGpMsg!.length,
+          itemBuilder: (context, index) {
             final groupMsgId = snapshot!.unreadGpMsg![index].id;
             final tUserChannelIds = snapshot!.t_user_channel_ids!.toList();
-            String name =
-                  snapshot!.unreadGpMsg![index].name.toString();
-               List<String> initials = name.split(" ").map((e) => e.substring(0, 1)).toList();
-              String gp_name = initials.join("");
-              String channelName = snapshot!
-                  .unreadGpMsg![index].channel_name
-                  .toString();
-              String groupMessage = snapshot!
-                  .unreadGpMsg![index].groupmsg
-                  .toString();
-              String gp_message_t = snapshot!
-                  .unreadGpMsg![index].created_at
-                  .toString();
-              DateTime time =
-                  DateTime.parse(gp_message_t).toLocal();
-              String createdAt =
-                  DateFormat('MMM d, yyyy hh:mm a').format(time);
-              bool shouldDisplay = false;
-            for(var tUserChannelId in tUserChannelIds){
-              if(int.parse(tUserChannelId) == groupMsgId){  
+            String name = snapshot!.unreadGpMsg![index].name.toString();
+            List<String> initials =
+                name.split(" ").map((e) => e.substring(0, 1)).toList();
+            String gp_name = initials.join("");
+            String channelName =
+                snapshot!.unreadGpMsg![index].channel_name.toString();
+            String groupMessage =
+                snapshot!.unreadGpMsg![index].groupmsg.toString();
+            String gp_message_t =
+                snapshot!.unreadGpMsg![index].created_at.toString();
+            DateTime time = DateTime.parse(gp_message_t).toLocal();
+            String createdAt = DateFormat('MMM d, yyyy hh:mm a').format(time);
+            bool shouldDisplay = false;
+            for (var tUserChannelId in tUserChannelIds) {
+              if (int.parse(tUserChannelId) == groupMsgId) {
                 shouldDisplay = true;
-              }           
+              }
             }
-            if(shouldDisplay){
+            if (shouldDisplay) {
               return Container(
-                          padding: const EdgeInsets.only(top: 10),
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    height: 50,
-                                    width: 50,
-                                    decoration: BoxDecoration(
-                                      color: Colors.amber,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                              
-                                    child:FittedBox(
-                                      alignment: Alignment.center,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(1.0),
-                                        child: Text(gp_name.toUpperCase(),
-                                        style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-                                      ),
-                                    ),                                
-                                  ),
-                                  const SizedBox( height: 22)
-                                  ],                            
-                                ),
-                              SizedBox(width: 5),
-                              Container(
-                                width: MediaQuery.of(context).size.width*0.7,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.shade300,
-                                  borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(10),
-                                    bottomLeft: Radius.circular(10),
-                                    bottomRight: Radius.circular(10)
-                                  ),        
-                                ),
-                                child:  Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(channelName,
-                                          style:TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                                          ),
-                                          Container(
-                                            width: MediaQuery.of(context).size.width*0.5,
-                                            child: Text(groupMessage,
-                                              style: const TextStyle(fontSize: 15)),                              
-                                            ),                     
-                                          Text(createdAt,style: TextStyle(fontSize: 10),)                   
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),        
-                              )
-                            ],
+                padding: const EdgeInsets.only(top: 10),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        );
-            }
-            else{
+                          child: FittedBox(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.all(1.0),
+                              child: Text(
+                                gp_name.toUpperCase(),
+                                style: const TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 22)
+                      ],
+                    ),
+                    SizedBox(width: 5),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  channelName,
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: Text(groupMessage,
+                                      style: const TextStyle(fontSize: 15)),
+                                ),
+                                Text(
+                                  createdAt,
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            } else {
               return Container();
             }
           },
-          
-        ) 
-        );
+        ));
   }
 }
